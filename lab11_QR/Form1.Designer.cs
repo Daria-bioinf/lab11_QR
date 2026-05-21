1d134ab47c9e8c05f22115cef23831d7d81456da
Daria Piskovtseva <d.piskovtseva@gmail.com>
2026-05-21 11:10:49 +0200

    koniec zajęcia 11:10

--- lab11_QR/Form1.Designer.cs
+++ lab11_QR/Form1.Designer.cs
@@ -30,79 +30,201 @@
         {
             this.textBoxID = new System.Windows.Forms.TextBox();
             this.textBoxName = new System.Windows.Forms.TextBox();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBoxType = new System.Windows.Forms.ComboBox();
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBoxDesc = new System.Windows.Forms.RichTextBox();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.pictureBoxQR = new System.Windows.Forms.PictureBox();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonLoad = new System.Windows.Forms.Button();
+            this.buttonQR = new System.Windows.Forms.Button();
+            this.buttonPNG = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxQR)).BeginInit();
             this.SuspendLayout();
             // 
             // textBoxID
             // 
-            this.textBoxID.Location = new System.Drawing.Point(140, 33);
+            this.textBoxID.Location = new System.Drawing.Point(50, 59);
             this.textBoxID.Name = "textBoxID";
             this.textBoxID.Size = new System.Drawing.Size(100, 22);
             this.textBoxID.TabIndex = 0;
             // 
             // textBoxName
             // 
-            this.textBoxName.Location = new System.Drawing.Point(140, 87);
+            this.textBoxName.Location = new System.Drawing.Point(50, 122);
             this.textBoxName.Name = "textBoxName";
             this.textBoxName.Size = new System.Drawing.Size(100, 22);
             this.textBoxName.TabIndex = 1;
             // 
-            // comboBox1
+            // comboBoxType
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(140, 139);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 24);
-            this.comboBox1.TabIndex = 2;
+            this.comboBoxType.FormattingEnabled = true;
+            this.comboBoxType.Items.AddRange(new object[] {
+            "DNA",
+            "RNA",
+            "Białko",
+            "Inny"});
+            this.comboBoxType.Location = new System.Drawing.Point(45, 179);
+            this.comboBoxType.Name = "comboBoxType";
+            this.comboBoxType.Size = new System.Drawing.Size(121, 24);
+            this.comboBoxType.TabIndex = 2;
             // 
             // dateTimePicker1
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(140, 189);
+            this.dateTimePicker1.Location = new System.Drawing.Point(22, 251);
             this.dateTimePicker1.Name = "dateTimePicker1";
             this.dateTimePicker1.Size = new System.Drawing.Size(200, 22);
             this.dateTimePicker1.TabIndex = 3;
             // 
-            // richTextBox1
+            // richTextBoxDesc
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(140, 236);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(100, 96);
-            this.richTextBox1.TabIndex = 4;
-            this.richTextBox1.Text = "";
+            this.richTextBoxDesc.Location = new System.Drawing.Point(22, 325);
+            this.richTextBoxDesc.Name = "richTextBoxDesc";
+            this.richTextBoxDesc.Size = new System.Drawing.Size(173, 85);
+            this.richTextBoxDesc.TabIndex = 4;
+            this.richTextBoxDesc.Text = "";
+            this.richTextBoxDesc.TextChanged += new System.EventHandler(this.richTextBoxDesc_TextChanged);
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(140, 374);
+            this.dataGridView1.Location = new System.Drawing.Point(333, 27);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(897, 402);
             this.dataGridView1.TabIndex = 5;
             // 
             // pictureBoxQR
             // 
-            this.pictureBoxQR.Location = new System.Drawing.Point(646, 153);
+            this.pictureBoxQR.Location = new System.Drawing.Point(22, 437);
             this.pictureBoxQR.Name = "pictureBoxQR";
-            this.pictureBoxQR.Size = new System.Drawing.Size(100, 50);
+            this.pictureBoxQR.Size = new System.Drawing.Size(183, 128);
+            this.pictureBoxQR.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBoxQR.TabIndex = 6;
             this.pictureBoxQR.TabStop = false;
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(201, 325);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(126, 34);
+            this.buttonSave.TabIndex = 7;
+            this.buttonSave.Text = "Zapisz";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // buttonLoad
+            // 
+            this.buttonLoad.Location = new System.Drawing.Point(201, 367);
+            this.buttonLoad.Name = "buttonLoad";
+            this.buttonLoad.Size = new System.Drawing.Size(126, 33);
+            this.buttonLoad.TabIndex = 8;
+            this.buttonLoad.Text = "Pokaż";
+            this.buttonLoad.UseVisualStyleBackColor = true;
+            this.buttonLoad.Click += new System.EventHandler(this.buttonLoad_Click);
+            // 
+            // buttonQR
+            // 
+            this.buttonQR.Location = new System.Drawing.Point(955, 453);
+            this.buttonQR.Name = "buttonQR";
+            this.buttonQR.Size = new System.Drawing.Size(147, 38);
+            this.buttonQR.TabIndex = 9;
+            this.buttonQR.Text = "Generuj QR";
+            this.buttonQR.UseVisualStyleBackColor = true;
+            this.buttonQR.Click += new System.EventHandler(this.buttonQR_Click);
+            // 
+            // buttonPNG
+            // 
+            this.buttonPNG.Location = new System.Drawing.Point(955, 497);
+            this.buttonPNG.Name = "buttonPNG";
+            this.buttonPNG.Size = new System.Drawing.Size(147, 40);
+            this.buttonPNG.TabIndex = 10;
+            this.buttonPNG.Text = "Eksport PNG";
+            this.buttonPNG.UseVisualStyleBackColor = true;
+            this.buttonPNG.Click += new System.EventHandler(this.buttonPNG_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(73, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 16);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "ID próbki";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(61, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 16);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Nazwa próbki";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(73, 160);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 16);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Typ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(73, 222);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(93, 16);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Data pobrania";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(73, 294);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(77, 16);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Opis/Uwagi";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(223, 464);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(68, 16);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "QR CODE";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1286, 536);
+            this.ClientSize = new System.Drawing.Size(1296, 624);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonPNG);
+            this.Controls.Add(this.buttonQR);
+            this.Controls.Add(this.buttonLoad);
+            this.Controls.Add(this.buttonSave);
             this.Controls.Add(this.pictureBoxQR);
             this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.richTextBoxDesc);
             this.Controls.Add(this.dateTimePicker1);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.comboBoxType);
             this.Controls.Add(this.textBoxName);
             this.Controls.Add(this.textBoxID);
             this.Name = "Form1";
@@ -118,11 +240,21 @@
 
         private System.Windows.Forms.TextBox textBoxID;
         private System.Windows.Forms.TextBox textBoxName;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBoxType;
         private System.Windows.Forms.DateTimePicker dateTimePicker1;
-        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBoxDesc;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.PictureBox pictureBoxQR;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button buttonLoad;
+        private System.Windows.Forms.Button buttonQR;
+        private System.Windows.Forms.Button buttonPNG;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
     }
 }
 
--- lab11_QR/Form1.cs
+++ lab11_QR/Form1.cs
@@ -2,19 +2,137 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SQLite;
 using System.Drawing;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using QRCoder;
 
 namespace lab11_QR
 {
     public partial class Form1 : Form
     {
+        string connectionString = "Data Source=Samples.db;Version=3;";
         public Form1()
         {
             InitializeComponent();
+
+            CreateTable();
+        }
+        private void CreateTable() {
+            using (SQLiteConnection connection = new SQLiteConnection(connectionString)) { 
+                connection.Open();
+                string query = @"CREATE TABLE IF NOT EXISTS Samples (
+                                Id TEXT PRIMARY KEY,
+                                Name TEXT, 
+                                Type TEXT,
+                                SampleDate TEXT,
+                                Description TEXT)";
+             SQLiteCommand command = new SQLiteCommand(query, connection);
+                command.ExecuteNonQuery();
+            }
+        
+        }
+
+        private void richTextBoxDesc_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+
+                string query =@"INSERT INTO Samples( ID,Name,Type,SampleDate,Description) VALUES(@id,@name,@type,@date,@desc)";
+
+                SQLiteCommand command =
+                new SQLiteCommand(query, connection);
+
+                command.Parameters.AddWithValue(
+                "@id",
+                textBoxID.Text);
+
+                command.Parameters.AddWithValue(
+                "@name",
+                textBoxName.Text);
+
+                command.Parameters.AddWithValue(
+                "@type",
+                comboBoxType.Text);
+
+                command.Parameters.AddWithValue(
+                "@date",
+                dateTimePicker1.Value.ToShortDateString());
+
+                command.Parameters.AddWithValue(
+                "@desc",
+                richTextBoxDesc.Text);
+
+                command.ExecuteNonQuery();
+
+                MessageBox.Show(
+                "Próbka została zapisana");
+            }
+        }
+
+        private void buttonLoad_Click(object sender, EventArgs e)
+        {
+            using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = "SELECT * FROM Samples";
+
+                SQLiteDataAdapter adapter = new SQLiteDataAdapter( query, connection);
+
+                DataTable table = new DataTable();
+
+                adapter.Fill(table);
+
+                dataGridView1.DataSource = table;
+            }
+        }
+
+        private void buttonQR_Click(object sender, EventArgs e)
+        {
+            string qrText =$@"ID: {textBoxID.Text} Nazwa: {textBoxName.Text} Typ: {comboBoxType.Text} Data: {dateTimePicker1.Value.ToShortDateString()}";
+
+            QRCodeGenerator generator = new QRCodeGenerator();
+
+            QRCodeData data =
+            generator.CreateQrCode(qrText, QRCodeGenerator.ECCLevel.Q);
+
+            QRCode qr = new QRCode(data);
+
+            Bitmap image = qr.GetGraphic(10);
+
+            pictureBoxQR.Image = image;
+        }
+
+        private void buttonPNG_Click(object sender, EventArgs e)
+        {
+            if (pictureBoxQR.Image == null)
+            {
+                MessageBox.Show("Najpierw wygeneruj QR");
+
+                return;
+            }
+
+            SaveFileDialog save =new SaveFileDialog();
+
+            save.Filter ="PNG (*.png)|*.png";
+
+            if (save.ShowDialog()== DialogResult.OK)
+            {
+                pictureBoxQR.Image.Save(save.FileName);
+
+                MessageBox.Show("PNG zapisany");
+            }
         }
     }
 }
